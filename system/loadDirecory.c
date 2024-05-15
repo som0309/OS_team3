303ecbcd7de2517ed35e3630248a3dd5b2f8bc56
5hseok <ohhs1010@gmail.com>
2024-05-16 01:16:59 +0900

    ✨ Feat : Directory 구조 가져오기 Directory.txt 파일에서 현재 정의된 파일들 가져와서 Tree 형태로 넣기

--- system/loadDirecory.c
+++ system/loadDirecory.c
@@ -2,7 +2,15 @@
 #include "../header/main.h"
 DirectoryTree* loadDirectory(){
 
-    DirectoryTree* currentDirTree;
+    DirectoryTree* currentDirTree = (DirectoryTree*)malloc(sizeof(DirectoryTree));
+    char tmp[256];
 
+    Directory = fopen("Directory.txt", "r");
+    while(fgets(tmp, 256, Directory) != NULL){
+        ReadNode(currentDirTree, tmp);
+    }
+    fclose(Directory);
+    currentDirTree->current = currentDirTree->root;
     return currentDirTree;
+
 }
