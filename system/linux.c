aef05feab70fc9a2e90b1dfc3cd2609a3e6b46f2
5hseok <ohhs1010@gmail.com>
2024-05-17 03:27:01 +0900

    ✨ Feat: 로그인 및 프롬프트 출력 기능 추가
    login 코드는 현재 로그인 가능한 사용자 리스트를 보여주고 사용자가 올바른 사용자 이름을 입력했다면 그 사용자로 로그인함
    printPrompt 코드는 로그인 후의 커서와 글씨, 현재 파일 위치 등을 출력함

--- system/linux.c
+++ system/linux.c
@@ -1,1 +1,94 @@
+#include "../header/main.h"
 //로그인, 로그아웃, 프롬프트 메세지 등을 출력하는 코드들 
+void login(UserList *userList, DirectoryTree *currentDirectoryTree) {
+    UserNode *tempUser = NULL;
+    char userName[MAX_NAME];
+    char temp[MAX_DIR];
+
+    tempUser = userList->head;
+    printf("%s\n",userList->head->name);
+    printf("Users: ");
+    
+    printf("%s\n", tempUser->name);
+    while (tempUser) {
+        printf("%s ", tempUser->name);
+        tempUser = tempUser->nextNode;
+    }
+    printf("\n");
+    while (1) {
+        printf("Login as: ");
+        fgets(userName, sizeof(userName), stdin);
+        userName[strlen(userName) - 1] = '\0';
+        if (!strcmp(userName, "exit")) {
+            exit(0);
+        }
+        tempUser = userExistence(userList, userName);
+        if (tempUser) {
+            userList->current = tempUser;
+            break;
+        }
+        printf("'%s' User not found\n", userName);
+    }
+    strcpy(temp, userList->current->dir);
+    movePath(currentDirectoryTree, temp);
+}
+
+// prompt를 출력하는 함수
+void printPrompt(DirectoryTree *currentDirectoryTree, Stack *dirStack) {
+    DirectoryNode *tempNode = NULL;
+    char temp[MAX_DIR] = "";
+    char tempSecond[MAX_DIR] = "";
+    char printCursor;
+
+    if (userList->current == userList->head) {
+        printCursor = '#'; // root로 로그인 했을 떄
+        DEFAULT;
+        printf("%s@OS_01_team3", userList->current->name);
+        printf(":");
+    }
+    else {
+        printCursor = '$'; // 그 외 user로 로그인 했을 때
+        BOLD;GREEN;
+        printf("%s@OS_01_team3", userList->current->name);
+        DEFAULT;
+        printf(":");
+    }
+    tempNode = currentDirectoryTree->current;
+
+    if(tempNode == currentDirectoryTree->root){
+        strcpy(temp, "/");
+    } else {
+        while (tempNode->parent) {
+            Push(dirStack, tempNode->name);
+            tempNode = tempNode->parent;
+        }
+        while (IsEmpty(dirStack) == 0) {
+            strcat(temp, "/");
+            strcat(temp, Pop(dirStack));
+        }
+    }
+    strncpy(tempSecond, temp, strlen(userList->current->dir));
+    if (userList->current == userList->head){
+        DEFAULT;
+        printf("%s", temp);
+    } else if (strcmp(userList->current->dir, tempSecond)) {
+	    BOLD;BLUE;
+        printf("%s", temp);
+    } else {
+        tempNode = currentDirectoryTree->current;
+        while (tempNode->parent) {
+            Push(dirStack, tempNode->name);
+            tempNode = tempNode->parent;
+        }
+        Pop(dirStack);
+        Pop(dirStack);
+	    BOLD;BLUE;
+        printf("~");
+        while (IsEmpty(dirStack) == 0) {
+            printf("/");
+            printf("%s", Pop(dirStack));
+        }
+    }
+    DEFAULT;
+    printf("%c ", printCursor);
+}
